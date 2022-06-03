7f90687498c393332b6243c5d592137f57eef361
vuke123 <lv53072@fer.hr>
2022-06-03 14:23:11 +0200

    SettingsVC layout
    functionality not done yet
    
    Co-Authored-By: AntePrpic <58590535+AntePrpic@users.noreply.github.com>

--- NBA App/Controller/SettingsVC.swift
+++ NBA App/Controller/SettingsVC.swift
@@ -14,18 +14,51 @@ class SettingsVC: UIViewController {
     
     private let navigationView = NavigationView(title1:"Settings")
 
+    private var deleteFavs = UILabel(frame: CGRect(x: 11, y: 455, width: 300, height: 30))
+    private var aboutView = AboutView(titleText: "About Luka Vukelić") //constraints in function bellow
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = .white
+        style()
         addSubviews()
         addConstraints()
-    
+        self.setupDeleteTap()
         self.navigationController?.isNavigationBarHidden = true
-        
+        self.aboutView.paragraph.numberOfLines = 0
     }
     
+    @objc func deleteFavorites(_ sender:
+            UITapGestureRecognizer) {
+        print("delete Favorites")
+//        delete favorites from user defaults
+    }
+    
+    func setupDeleteTap(){
+        let deleteTap = UITapGestureRecognizer(target: self, action: #selector(self.deleteFavorites(_:)))
+        self.deleteFavs.isUserInteractionEnabled = true
+        self.deleteFavs.addGestureRecognizer(deleteTap)
+    }
+    
+    
+    func style(){
+        deleteFavs.layer.borderColor = UIColor.red.cgColor
+        deleteFavs.layer.borderWidth = 3.0
+        deleteFavs.text = "CLEAR MY FAVORITES LIST"
+        deleteFavs.textAlignment = .center
+        deleteFavs.textColor = .red
+        deleteFavs.layer.cornerRadius = 10
+        aboutView.layer.borderColor = UIColor.gray.cgColor
+        aboutView.backgroundColor = .systemGray4
+        aboutView.layer.borderWidth = 0.0
+        aboutView.layer.masksToBounds = true
+        aboutView.layer.cornerRadius = 10
+
+    }
     func addSubviews(){
         view.addSubview(navigationView)
+        view.addSubview(deleteFavs)
+        view.addSubview(aboutView)
     }
     
     func addConstraints(){
@@ -34,6 +67,19 @@ class SettingsVC: UIViewController {
             $0.width.equalToSuperview()
             $0.top.equalToSuperview()
         }
+        aboutView.snp.makeConstraints{
+            $0.height.equalTo(140)
+            $0.width.equalTo(300)
+            $0.bottom.equalTo(deleteFavs.snp.top).offset(-10)
+            $0.centerX.equalToSuperview()
+        }
+        
+//        deleteFavs.snp.makeConstraints{
+//            $0.height.equalTo(20)
+//            $0.width.equalTo(100)
+//            $0.bottom.equalToSuperview().offset(15)
+//            $0.centerX.equalToSuperview()
+//        }  CGRect(x:11, y:345, width:300, height: 100))
     }
 
 
---
+++ NBA App/View/AboutView.swift
@@ -0,0 +1,70 @@
+//
+//  AboutLabel.swift
+//  NBA App
+//
+//  Created by Luka Vukelić on 03.06.2022..
+//
+
+import Foundation
+import UIKit
+
+class AboutView : UIView {
+    
+    let title = UILabel()
+    let paragraph = UILabel()
+    let moreInfo = UILabel()
+    
+    private var titleText: String?
+    
+    init(titleText: String){
+        super.init(frame: .zero)
+        self.titleText = titleText
+        style()
+        addSubviews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   func style(){
+        title.text = titleText
+        title.textColor = .black
+        title.font = .boldSystemFont(ofSize: 20)
+       paragraph.text = "Computing student developing skills in iOS. Born in Rijeka, lives in Zagreb, in free time love being in nature."
+       paragraph.textColor = .black
+       paragraph.font = paragraph.font.withSize(15)
+       moreInfo.text = "MORE INFO"
+       moreInfo.textColor = .blue
+    }
+   func addSubviews(){
+       addSubview(title)
+       addSubview(paragraph)
+       addSubview(moreInfo)
+    }
+   func addConstraints(){
+       title.snp.makeConstraints{
+           $0.top.equalToSuperview().offset(10)
+           $0.leading.equalToSuperview().offset(15)
+           $0.width.equalTo(190)
+           $0.height.equalTo(20)
+       }
+       paragraph.snp.makeConstraints{
+           $0.top.equalTo(title.snp.bottom).offset(-5)
+           $0.leading.equalToSuperview().offset(15)
+           $0.height.equalTo(80)
+           $0.width.equalTo(280)
+       }
+       moreInfo.snp.makeConstraints{
+           $0.top.equalTo(paragraph.snp.bottom)
+           $0.height.equalTo(30)
+           $0.width.equalTo(280)
+           $0.leading.equalToSuperview().offset(15)
+       }
+       
+       
+   }
+    
+}
+
